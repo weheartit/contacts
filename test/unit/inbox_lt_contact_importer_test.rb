aeb58d4f5208455b2b1962dadfa281e46ae1ce51
Laurynas Butkus <laurynas.butkus@gmail.com>
2012-03-19 18:02:19 +0200

    inbox.lt connector

---
+++ test/unit/inbox_lt_contact_importer_test.rb
@@ -0,0 +1,39 @@
+dir = File.dirname(__FILE__)
+require "#{dir}/../test_helper"
+require 'contacts'
+
+class InboxLtContactImporterTest < ContactImporterTestCase
+  def setup
+    super
+    @account = TestAccounts[:inbox_lt]
+  end
+
+  def test_successful_login
+    Contacts.new(:inbox_lt, @account.username, @account.password)
+  end
+
+  def test_importer_fails_with_invalid_password
+    assert_raise(Contacts::AuthenticationError) do
+      Contacts.new(:inbox_lt, @account.username, "wrong_password")
+    end
+  end
+
+  def test_importer_fails_with_blank_password
+    assert_raise(Contacts::AuthenticationError) do
+      Contacts.new(:inbox_lt, @account.username, "")
+    end
+  end
+
+  def test_importer_fails_with_blank_username
+    assert_raise(Contacts::AuthenticationError) do
+      Contacts.new(:inbox_lt, "", @account.password)
+    end
+  end
+
+  def test_fetch_contacts
+    contacts = Contacts.new(:inbox_lt, @account.username, @account.password).contacts
+    @account.contacts.each do |contact|
+      assert contacts.include?(contact), "Could not find: #{contact.inspect} in #{contacts.inspect}"
+    end
+  end
+end

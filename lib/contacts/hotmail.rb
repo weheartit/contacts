681a76ef3989fcf51f5a1fb52d9bc1168f5c42b6
Wong Liang Zan <zan@liangzan.net>
2012-11-03 17:26:26 +0800

    encoding with utf-8

--- lib/contacts/hotmail.rb
+++ lib/contacts/hotmail.rb
@@ -64,7 +64,7 @@ class Contacts
         contact_list_url = get_contact_list_url
         data, resp, cookies, forward = get(contact_list_url, @cookies )
 
-        data.force_encoding('ISO-8859-1')
+        data.force_encoding('UTF-8')
         @contacts = CSV.parse(data, {:headers => true, :col_sep => data[7]}).map do |row|
           name = ""
           name = row["First Name"] if !row["First Name"].nil?

04ed2d1ad040e39520b898a199898632f873386c
Wong Liang Zan <zan@liangzan.net>
2013-02-05 14:23:54 +0800

    applied the patch from https://github.com/rallysf/contacts/commit/3a3f1c269235d9734feb1724103582c0cb4f9d3f

--- lib/contacts/hotmail.rb
+++ lib/contacts/hotmail.rb
@@ -64,7 +64,8 @@ class Contacts
         contact_list_url = get_contact_list_url
         data, resp, cookies, forward = get(contact_list_url, @cookies )
 
-        data.force_encoding('UTF-8')
+        data.force_encoding('UTF-16')
+        data = data.encode('UTF-8')
         @contacts = CSV.parse(data, {:headers => true, :col_sep => data[7]}).map do |row|
           name = ""
           name = row["First Name"] if !row["First Name"].nil?
